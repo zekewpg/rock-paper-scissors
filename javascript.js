4e63f162ff6c4154f4264dbc63f3dcf8c58c5c80
Robert Szeryk <8922590+zekewpg@users.noreply.github.com>
2023-05-16 21:47:05 +0200

    Finshed playRPS() function should return a string indicating winner, loser, and tie.

--- javascript.js
+++ javascript.js
@@ -8,16 +8,49 @@ function playRPS(playerChoice, computerChoice) {
 
     // filter for invalid values and return an error message if that is the case
     if (playerChoice && computerChoice) {
-        // we should not get to this point if there are not two defined/not null inputs
-        // so we are now playing RPS!
+
+        // add lower case variables to cut down on code
+        let lowerPlayer = String(playerChoice).toLowerCase();
+        let lowerComputer = String(computerChoice).toLowerCase();
         
-        console.log(playerChoice + ' ' + computerChoice);
+        // we should not get to this point if there are not two defined/not null inputs
+        // need to check for valid choices
+        // what is most efficient way to check that booth variables are valid
+        // ie rock or paper or scissors. use array include! should this be a seperate function?
+        const choiceArray = ['rock','paper','scissors'];
+        if (choiceArray.includes(lowerPlayer) && choiceArray.includes(lowerComputer)) {
+            // two valid entries have been provided. Now we need to select winner and return info/winner
+            // check to see if player won
+            const weapons = {
+                rock: {weakTo: 'paper', strongTo: 'scissors'},
+                paper: {weakTo: 'scissors', strongTo: 'rock'},
+                scissors: {weakTo: 'rock', strongTo: 'paper'}
+            }
+
+            if (weapons[lowerPlayer].strongTo === lowerComputer) {
+                // Player won because `${lowerPlayer} beats ${lowerCopmuter}
+                return `Player won! ${lowerPlayer} beats ${lowerComputer}.`;
+                
+            }
+
+            if (weapons[lowerPlayer].weakTo === lowerComputer) {
+                // player lost
+                return `Computer won! ${lowerComputer} beats ${lowerPlayer}.`;
+            }
+            
+            // tie
+            return `It was a tie with ${lowerPlayer} and ${lowerComputer}. Try again!`;
+            
+        }  else {
+            // entries provided were not valid (rock/paper/scissors) should i check up front with nulls/undefined?
+            // return error message
+             console.log('Game cannot be played without valid throws from the player and the computer. rock/paper/scissors');
+        }     
     } else {
         // two defined/non null inputs were not provided
         console.log('Game cannot be played without throws from both the player and the computer');
     }
 }
-
 
 function getComputerChoice() {
 
